f7a6945424629f610ca3eabe75996a41b9aba907
AFarforov <anton@black-sheep.ru>
2011-12-19 23:21:44 +0400

    INITIAL

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,9 +2,11 @@
 //= require jquery_ujs
 
 //= require_tree
+//= require aloha
 
 //= require gmaps4rails/bing.js
 //= require gmaps4rails/googlemaps.js
 //= require gmaps4rails/mapquest.js
 //= require gmaps4rails/openlayers.js
 //= require gmaps4rails/all_apis.js
+
