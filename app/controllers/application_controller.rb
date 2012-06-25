deea55c7aef5e20a6c9e06007525374058f09eeb
AFarforov <anton@black-sheep.ru>
2012-06-25 21:26:26 +0400

    pre beta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,23 +18,21 @@ class ApplicationController < ActionController::Base
   def build_menu
     @about_menu = Static.find_all_by_menu("about")
     @services_menu = Static.find_all_by_menu("services")
-    #@address = Static.where( :type => "address")
 
     if controller_name == "registrations"
-
       unless current_user.nil?
         if current_user.manager?
           @flats = Flat.order("updated_at desc")
           @tenders = Tender.where("client_status_id <> ?", 3).order("updated_at desc")
         end
-        if current_user.rieltor?
-          @flats = Flat.where("agent_id = ?", current_user.id).order("updated_at desc")
-          @tenders = Tender.where("agent_id = ? && client_status_id <> ?", current_user.id, 3).order("updated_at desc")
-        end
-        if current_user.client?
-          @flats = Flat.order("updated_at desc")
-          @tenders = Tender.where("client_status_id <> ?", 3).order("updated_at desc")
-        end
+        #if current_user.rieltor?
+        #  @flats = Flat.where("agent_id = ?", current_user.id).order("updated_at desc")
+        #  @tenders = Tender.where("agent_id = ? && client_status_id <> ?", current_user.id, 3).order("updated_at desc")
+        #end
+        #if current_user.client?
+        #  @flats = Flat.order("updated_at desc")
+        #  @tenders = Tender.where("client_status_id <> ?", 3).order("updated_at desc")
+        #end
       end
     end
   end
